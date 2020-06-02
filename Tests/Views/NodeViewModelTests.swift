6a8adb060da3ca2690994d4640c5a175b1930036
Nicolas Mandica <nico.mandica@orange.fr>
2020-06-03 01:08:32 +0200

    Add an empty line in NodeViewModelTests

--- Tests/Views/NodeViewModelTests.swift
+++ Tests/Views/NodeViewModelTests.swift
@@ -4,6 +4,7 @@ import XCTest
 class NodeViewModelTests: XCTestCase {
     func testInit() {
         let node = SimpleNode(id: "2", group: 3)
+        
         let nodeViewModel = NodeViewModel(node)
         
         XCTAssertEqual(nodeViewModel.id, "2")
