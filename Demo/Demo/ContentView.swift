9ebbf3343220f98e25dbe6759ff42f7ea2492662
Nicolas Mandica <nico.mandica@orange.fr>
2020-06-02 12:47:24 +0200

    Add Demo project

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+import DirectedGraph
+
+struct ContentView: View {
+    static private let graph = try! SimpleGraph.load(filename: "graph")
+    private let viewModel = GraphViewModel(Self.graph)
+    
+    var body: some View {
+        VStack {
+            GraphView(viewModel)
+            
+            HStack {
+                Button("Auto Layout") {
+                    self.viewModel.autoLayout()
+                }
+                
+                Spacer()
+                
+                Button("Toggle Edge Values") {
+                    self.viewModel.toggleEdgeValues()
+                }
+            }.padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
