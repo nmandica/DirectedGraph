148d3e347fe6133585981a9aa3dddb402f6ff056
Nicolas Mandica <nico.mandica@orange.fr>
2020-06-10 15:22:11 +0200

    Add unit tests to EdgeViewModel

--- Tests/Views/EdgeViewModelTests.swift
+++ Tests/Views/EdgeViewModelTests.swift
@@ -2,16 +2,40 @@ import XCTest
 @testable import DirectedGraph
 
 class EdgeViewModelTests: XCTestCase {
+    private let sourceNode = NodeViewModel(SimpleNode(id: "1", group: 3))
+    private let targetNode = NodeViewModel(SimpleNode(id: "2", group: 3))
+    private var cancellable: Any?
+    
     func testEndIsNotNaNWhenSourceAndTargetAtSamePosition() {
-        let node1 = SimpleNode(id: "1", group: 3)
-        let node2 = SimpleNode(id: "2", group: 3)
-        let nodeViewModel1 = NodeViewModel(node1)
-        let nodeViewModel2 = NodeViewModel(node2)
-        nodeViewModel1.position = CGPoint(25, 40)
-        nodeViewModel2.position = CGPoint(25, 40)
+        sourceNode.position = CGPoint(25, 40)
+        targetNode.position = CGPoint(25, 40)
         
-        let viewModel = EdgeViewModel(source: nodeViewModel1, target: nodeViewModel2, value: .zero)
+        let viewModel = EdgeViewModel(source: sourceNode, target: targetNode, value: .zero)
         
         XCTAssertFalse(viewModel.end.x.isNaN)
+    }
+    
+    func testSourceChangeTriggerEdgeChange() {
+        let viewModel = EdgeViewModel(source: sourceNode, target: targetNode, value: 40)
+        var hasChanged = false
+        cancellable = viewModel.objectWillChange.sink { _ in
+            hasChanged = true
+        }
+        
+        sourceNode.position.x += 2
+        
+        XCTAssertTrue(hasChanged)
+    }
+    
+    func testTargetChangeTriggerEdgeChange() {
+        let viewModel = EdgeViewModel(source: sourceNode, target: targetNode, value: 40)
+        var hasChanged = false
+        cancellable = viewModel.objectWillChange.sink { _ in
+            hasChanged = true
+        }
+        
+        targetNode.position.x += 2
+        
+        XCTAssertTrue(hasChanged)
     }
 }
